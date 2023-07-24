fbd5a4c14d8aab38a7dcf3e544885794eb109cee
taufiqurSabbir <taufiqur.sabbir11@gmail.com>
2023-07-25 00:35:54 +0600

    login page ini

--- lib/UI/screens/loginScreen.dart
+++ lib/UI/screens/loginScreen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:task_managment/UI/utils/asset_utils.dart';
 
 
 class Login extends StatefulWidget {
@@ -12,7 +14,31 @@ class _LoginState extends State<Login> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+          body: Stack(
+            children: [
+              SizedBox(
+                height: double.infinity,
+                width: double.infinity,
+                child: SvgPicture.asset(
+                  AssetUtils.BackgroundSVG,
+                  fit: BoxFit.fitHeight,
+                ),
+              ),
 
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SvgPicture.asset(
+                    AssetUtils.logoSvg,height: 100,width: 50,
+                  ),
+                  TextFormField(),
+                  TextFormField(),
+                  ElevatedButton(onPressed: (){}, child: Text('Login'))
+                ],
+
+              )
+            ],
+          ),
     );
   }
 }
