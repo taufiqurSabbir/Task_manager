be93a91cdab4aa786f1934d333b118444b15be4c
taufiqurSabbir <taufiqur.sabbir11@gmail.com>
2023-07-25 23:49:42 +0600

    email screen

---
+++ lib/UI/screens/email_varification_screen.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/material.dart';
+import 'package:task_managment/UI/widget/screen_background.dart';
+
+class email_varification extends StatefulWidget {
+  const email_varification({Key? key}) : super(key: key);
+
+  @override
+  State<email_varification> createState() => _email_varificationState();
+}
+
+class _email_varificationState extends State<email_varification> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: screen_background(
+        child: SingleChildScrollView(
+          child: SafeArea(
+            child: Padding(
+              padding: EdgeInsets.all(24.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  SizedBox(
+                    height: 70,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(bottom: 15.0),
+                    child: Align(
+                      alignment: Alignment.topLeft,
+                      child: Text('Get Started with',
+                          style: Theme.of(context).textTheme.titleLarge),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(bottom: 15.0),
+                    child: Align(
+                      alignment: Alignment.topLeft,
+                      child: Text(
+                          'A 6 digits pin will send to your email address',
+                          style: Theme.of(context)
+                              .textTheme
+                              .bodyMedium
+                              ?.copyWith(color: Colors.grey)),
+                    ),
+                  ),
+                  TextFormField(
+                    keyboardType: TextInputType.emailAddress,
+                    decoration: const InputDecoration(hintText: 'Email'),
+                  ),
+                  SizedBox(
+                    height: 15,
+                  ),
+                  SizedBox(
+                    height: 15,
+                  ),
+                  SizedBox(
+                    width: double.infinity,
+                    child: ElevatedButton(
+                        onPressed: () {},
+                        child: const Icon(Icons.arrow_forward_ios_sharp)),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const Text(
+                        'Have an account',
+                        style: TextStyle(
+                            fontWeight: FontWeight.w500,
+                            color: Colors.black,
+                            fontSize: 15,
+                            letterSpacing: 0.5),
+                      ),
+                      TextButton(
+                          onPressed: () {
+                            Navigator.pop(context);
+                          },
+                          child: const Text('Sign In'))
+                    ],
+                  )
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/UI/screens/loginScreen.dart
+++ lib/UI/screens/loginScreen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:task_managment/UI/screens/email_varification_screen.dart';
 import 'package:task_managment/UI/widget/screen_background.dart';
 
 class Login extends StatefulWidget {
@@ -14,75 +15,91 @@ class _LoginState extends State<Login> {
   Widget build(BuildContext context) {
     return Scaffold(
         body: screen_background(
-      child: SafeArea(
-        child: Padding(
-          padding: EdgeInsets.all(24.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Padding(
-                padding: const EdgeInsets.only(bottom: 15.0),
-                child: Align(
-                  alignment: Alignment.topLeft,
-                  child: Text('Get Started with',
-                      style: Theme.of(context).textTheme.titleLarge),
+      child: SingleChildScrollView(
+        child: SafeArea(
+          child: Padding(
+            padding: EdgeInsets.all(24.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(
+                  height: 70,
                 ),
-              ),
-              TextFormField(
-                decoration: const InputDecoration(hintText: 'Email'),
-              ),
-              SizedBox(
-                height: 15,
-              ),
-              TextFormField(
-                decoration: InputDecoration(
-                    hintText: 'Password',
-                    suffixIcon: IconButton(
-                        onPressed: () {
-                          password = false;
-                          setState(() {});
-                          Future.delayed(Duration(seconds: 1)).then((value) {
-                            password = true;
-                            setState(() {});
-                          });
-                        },
-
-                        icon: Icon(Icons.remove_red_eye))),
-                obscureText: password,
-              ),
-              SizedBox(
-                height: 15,
-              ),
-              SizedBox(
-                width: double.infinity,
-                child: ElevatedButton(
-                    onPressed: () {},
-                    child: const Icon(Icons.arrow_forward_ios_sharp)),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(top: 18),
-                child: InkWell(
-                    onTap: () {},
-                    child: const Text(
-                      'Forget Password?',
-                      style: TextStyle(color: Colors.grey),
-                    )),
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  const Text(
-                    'Don\'t have an account',
-                    style: TextStyle(
-                        fontWeight: FontWeight.w500,
-                        color: Colors.black,
-                        fontSize: 15,
-                        letterSpacing: 0.5),
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 15.0),
+                  child: Align(
+                    alignment: Alignment.topLeft,
+                    child: Text('Get Started with',
+                        style: Theme.of(context)
+                            .textTheme
+                            .titleLarge),
                   ),
-                  TextButton(onPressed: () {}, child: const Text('Sign Up'))
-                ],
-              )
-            ],
+                ),
+                TextFormField(
+                  keyboardType: TextInputType.emailAddress,
+                  decoration: const InputDecoration(hintText: 'Email'),
+                ),
+                SizedBox(
+                  height: 15,
+                ),
+                TextFormField(
+                  decoration: InputDecoration(
+                      hintText: 'Password',
+                      suffixIcon: IconButton(
+                          onPressed: () {
+                            password = false;
+                            setState(() {});
+                            Future.delayed(Duration(seconds: 1)).then((value) {
+                              password = true;
+                              setState(() {});
+                            });
+                          },
+                          icon: Icon(Icons.remove_red_eye))),
+                  obscureText: password,
+                ),
+                SizedBox(
+                  height: 15,
+                ),
+                SizedBox(
+                  width: double.infinity,
+                  child: ElevatedButton(
+                      onPressed: () {},
+                      child: const Icon(Icons.arrow_forward_ios_sharp)),
+                ),
+                Center(
+                  child: Padding(
+                    padding: const EdgeInsets.only(top: 18),
+                    child: InkWell(
+                        onTap: () {},
+                        child: const Text(
+                          'Forget Password?',
+                          style: TextStyle(color: Colors.grey),
+                        )),
+                  ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Text(
+                      'Don\'t have an account',
+                      style: TextStyle(
+                          fontWeight: FontWeight.w500,
+                          color: Colors.black,
+                          fontSize: 15,
+                          letterSpacing: 0.5),
+                    ),
+                    TextButton(
+                        onPressed: () {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => email_varification()));
+                        },
+                        child: const Text('Sign Up'))
+                  ],
+                )
+              ],
+            ),
           ),
         ),
       ),
