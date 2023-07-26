f352beaeb658a9b570b85210fe5114ea5d688554
Taufiqur Rahman <62732164+taufiqurSabbir@users.noreply.github.com>
2023-07-26 15:42:08 +0600

    sign up page ui completed

--- lib/UI/screens/SignUp_screen.dart
+++ lib/UI/screens/SignUp_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:task_managment/UI/screens/loginScreen.dart';
 
 import '../widget/screen_background.dart';
 import 'email_varification_screen.dart';
@@ -11,86 +12,106 @@ class signup extends StatefulWidget {
 }
 
 class _signupState extends State<signup> {
+  bool password = true;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         body: screen_background(
-          child: SingleChildScrollView(
-            child: SafeArea(
-              child: Padding(
-                padding: EdgeInsets.all(24.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    SizedBox(
-                      height: 70,
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(bottom: 15.0),
-                      child: Align(
-                        alignment: Alignment.topLeft,
-                        child: Text('Get Started with',
-                            style: Theme.of(context)
-                                .textTheme
-                                .titleLarge),
-                      ),
-                    ),
-                    TextFormField(
-                      keyboardType: TextInputType.emailAddress,
-                      decoration: const InputDecoration(hintText: 'Email'),
-                    ),
-                    SizedBox(
-                      height: 15,
-                    ),
-
-                    SizedBox(
-                      height: 15,
-                    ),
-                    SizedBox(
-                      width: double.infinity,
-                      child: ElevatedButton(
-                          onPressed: () {},
-                          child: const Icon(Icons.arrow_forward_ios_sharp)),
-                    ),
-                    Center(
-                      child: Padding(
-                        padding: const EdgeInsets.only(top: 18),
-                        child: InkWell(
-                            onTap: () {
-                              Navigator.push(
-                                  context,
-                                  MaterialPageRoute(
-                                      builder: (context) => email_varification()));
-                            },
-                            child: const Text(
-                              'Forget Password?',
-                              style: TextStyle(color: Colors.grey),
-                            )),
-                      ),
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        const Text(
-                          'Don\'t have an account',
-                          style: TextStyle(
-                              fontWeight: FontWeight.w500,
-                              color: Colors.black,
-                              fontSize: 15,
-                              letterSpacing: 0.5),
-                        ),
-                        TextButton(
-                            onPressed: () {
-                              Navigator.push(context, MaterialPageRoute(builder: (context)=>signup()));
-                            },
-                            child: const Text('Sign Up'))
-                      ],
-                    )
-                  ],
+      child: SingleChildScrollView(
+        child: SafeArea(
+          child: Padding(
+            padding: EdgeInsets.all(24.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(
+                  height: 70,
                 ),
-              ),
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 15.0),
+                  child: Align(
+                    alignment: Alignment.topLeft,
+                    child: Text('Join With US',
+                        style: Theme.of(context).textTheme.titleLarge),
+                  ),
+                ),
+                TextFormField(
+                  keyboardType: TextInputType.emailAddress,
+                  decoration: const InputDecoration(hintText: 'Email'),
+                ),
+                SizedBox(
+                  height: 15,
+                ),
+                TextFormField(
+                  keyboardType: TextInputType.text,
+                  decoration: const InputDecoration(hintText: 'First Name'),
+                ),
+                SizedBox(
+                  height: 15,
+                ),
+                TextFormField(
+                  keyboardType: TextInputType.text,
+                  decoration: const InputDecoration(hintText: 'Last Name'),
+                ),
+                SizedBox(
+                  height: 15,
+                ),
+                TextFormField(
+                  keyboardType: TextInputType.number,
+                  decoration: const InputDecoration(hintText: 'Mobile'),
+                ),
+                SizedBox(
+                  height: 15,
+                ),
+                TextFormField(
+                  keyboardType: TextInputType.text,
+                  decoration: InputDecoration(
+                      hintText: 'Password',
+                      suffixIcon: IconButton(
+                          onPressed: () {
+                            password = false;
+                            setState(() {});
+                            Future.delayed(Duration(seconds: 2)).then((value) {
+                              password = true;
+                              setState(() {});
+                            });
+                          },
+                          icon: Icon(Icons.remove_red_eye_outlined))),
+                  obscureText: password,
+                ),
+                SizedBox(
+                  height: 15,
+                ),
+                SizedBox(
+                  width: double.infinity,
+                  child: ElevatedButton(
+                      onPressed: () {},
+                      child: const Icon(Icons.arrow_forward_ios_sharp)),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Text(
+                      'Alredy have an account?',
+                      style: TextStyle(
+                          fontWeight: FontWeight.w500,
+                          color: Colors.black,
+                          fontSize: 15,
+                          letterSpacing: 0.5),
+                    ),
+                    TextButton(
+                        onPressed: () {
+                          Navigator.push(context,
+                              MaterialPageRoute(builder: (context) => Login()));
+                        },
+                        child: const Text('Sign in'))
+                  ],
+                )
+              ],
             ),
           ),
-        ));
+        ),
+      ),
+    ));
   }
 }
